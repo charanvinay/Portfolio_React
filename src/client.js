54b2e1a262c6e8146056c4c359930df23fd86d2f
charanvinay <charanvinaynarni@gmail.com>
2022-07-07 01:16:56 +0530

    client

--- src/client.js
+++ src/client.js
@@ -2,11 +2,12 @@ import sanityClient from "@sanity/client";
 import imageUrlBuilder from "@sanity/image-url";
 
 export const client = sanityClient({
-  projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
+  projectId: "3i58cp7h",
   dataset: "production",
   apiVersion: "2022-02-01",
   useCdn: true,
-  token: process.env.REACT_APP_SANITY_TOKEN,
+  token:
+    "skJqR1mVpRD8RYYHzTJr86FyUFGZp1xNhOceymbKALkkdIG51SZ9OPpw4z6KFDxgxSv8hO3T61FbQTMiaFV7haO9O9c5nhlkohZ5qWjOazlcyAwU0qpUKd3JjDmyQ8xa07yWxczmIfaEsdFNawofEYPgaeTH4qJFmmh8LZdEfdsXF3tdaWbK",
 });
 
 const builder = imageUrlBuilder(client);
