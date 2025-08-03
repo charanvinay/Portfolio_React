8ffac6b1f5d52756c8695b28c460c1d62a19a8ef
charanvinay-ael <charanvinay@apexcura.com>
2025-08-03 19:44:08 +0530

    utils changed

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const BASE_URL = "https://portfolio-node-w0mg.onrender.com";
-const BASE_URL = "http://localhost:1234";
+const BASE_URL = "https://portfolio-node-w0mg.onrender.com";
+// const BASE_URL = "http://localhost:1234";
 const makeApiCall = async ({ endPoint = "", method = "GET", data = [] }) => {
   if (!endPoint) return { status: false, message: "Invalid Endpoint" };
   const url = `${BASE_URL}/api${endPoint}`;
